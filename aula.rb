3df02fdff131101cded9a35fb05c93903342770e
Rodrigokaff <rodrigo_miguel@outlook.com>
2018-06-02 19:44:12 -0300

    Aula 5 concluida

---
+++ aula.rb
@@ -0,0 +1,63 @@
+
+#############################CONCATENAR############################
+#a = "Rodrigo"
+
+# b = "Miguel"
+
+# b.replace("Martins")
+
+
+# puts b 
+# puts a + " " + b
+# puts " "
+# puts "Rodrigo #{b}"
+# puts " "
+# puts 'Rodrigo '.concat(b)
+
+
+###########################SUBSTRING##############################
+
+# a = "RodrigoMiguel"
+# puts " "
+# puts " "
+# puts a[3,6]
+# puts " "
+# puts " "
+
+# puts a [2]
+# puts a[0..4]
+
+##############################GSUB (Substituicao)#####################
+
+# a = "Rodrigo Miguel Gomes Rocha Silva"
+# puts " "
+# puts a
+# puts " "
+# puts a.gsub("Gomes", "Martins")
+# puts " "
+# puts a
+# puts " "
+# a = a.gsub("Gomes", "Martins")
+# a.gsub!("Gomes", "Martins")
+# puts " "
+# puts a
+# puts " "
+
+
+# a = "      Rodrigo Miguel       " 
+# b = "-"
+
+
+# puts b + a.strip + b
+# puts b + a.lstrip + b
+# puts b + a.rstrip + b
+
+
+# a = "rodrigo"
+
+# puts a.reverse 
+
+
+a = "Ruby on Rails"
+
+puts a.split " "
